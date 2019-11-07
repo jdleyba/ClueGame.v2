9a488c29128c0876f64edd8aedd3b783b364d8f1
jdleyba <jdleyba@mymail.mines.edu>
2019-11-06 19:33:34 -0700

    updated tests

--- ClueGame/src/clueGame/computerPlayer.java
+++ ClueGame/src/clueGame/computerPlayer.java
@@ -1,14 +1,38 @@
 package clueGame;
 
+import java.util.Iterator;
 import java.util.Set;
 
 public class computerPlayer extends Player {
 	private char lastVisited;
+	private BoardCell targetCell;
 	
 	public computerPlayer() {
 	}
 	public BoardCell pickLocation(Set<BoardCell> targets) {
-		return null;
+		
+		Iterator<BoardCell> iterator = targets.iterator();
+		while(iterator.hasNext()) {
+			BoardCell cell = iterator.next();
+			if(cell.isRoom()) {
+				if(cell.getInitial() != lastVisited) {
+					targetCell = cell;
+					return targetCell;
+				}
+			}
+		}
+		
+		Iterator<BoardCell> iterator2 = targets.iterator();
+		while(iterator2.hasNext()) {
+			BoardCell cell = iterator2.next();
+			if(cell.isRoom()) {
+				if(cell.getInitial() != lastVisited) {
+					targetCell = cell;
+					return targetCell;
+				}
+			}
+		}
+		return targetCell;
 	}
 	public void makeAccusation() {
 
