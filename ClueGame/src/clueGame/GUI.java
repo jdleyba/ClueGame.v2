a308b75d705cc4570f9bb48864a23bd2859dfeaa
jdleyba <jdleyba@mymail.mines.edu>
2019-11-16 17:50:51 -0700

    Working on GUI

--- ClueGame/src/clueGame/GUI.java
+++ ClueGame/src/clueGame/GUI.java
@@ -1,5 +1,13 @@
 package clueGame;
 
-public class GUI {
+import java.awt.GridLayout;
+import javax.swing.JPanel;
 
+public class GUI extends JPanel{
+	private int diceRoll;
+	private String whosTurn;
+	public GUI() {
+		setLayout(new GridLayout(2,0));
+		
+	}
 }
